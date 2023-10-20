470da28c65b2c7a7d268483a67e1bd1fde77ece3
Priya Ambulkar <ambulkarpriya4@gmail.com>
2023-10-20 23:26:28 +0530

    Feature added

--- Test/src/com/testgit/Example.java
+++ Test/src/com/testgit/Example.java
@@ -5,6 +5,7 @@ public class Example {
 	public static void main(String[] args) {
 
 		System.out.println("New Class 'Example' is Created");
+		System.out.println("Added a new code to feature branch");
 
 	}
 
